73df1dba5fe3b2f1cd897ab879e5b255681e54d3
Christophe Fergeau <cfergeau@redhat.com>
2023-04-24 17:47:45 +0200

    Add golangci-lint

---
+++ tools/tools.mk
@@ -0,0 +1,4 @@
+TOOLS_BINDIR = $(realpath $(TOOLS_DIR)/bin)
+
+$(TOOLS_BINDIR)/golangci-lint: $(TOOLS_DIR)/go.mod
+	cd $(TOOLS_DIR) && GOBIN="$(TOOLS_BINDIR)" go install github.com/golangci/golangci-lint/cmd/golangci-lint
