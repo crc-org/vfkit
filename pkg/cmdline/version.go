90e37b12212192cb05c6059c92d4922172cd67b3
Christophe Fergeau <cfergeau@redhat.com>
2023-11-23 15:12:49 +0100

    version: Add moduleVersionFromBuildInfo
    This adds correct version information for `vfkit -version` when
    it's installed using:
    ```
    go install github.com/crc-org/vfkit/cmd/vfkit
    ```
    
    However, due to missing entitlements, a binary installed this way is
    unlikely to be able to start VMs.
    I'm keeping this codepath to use the same code in gvisor-tap-vsock and
    vfkit.
    
    Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

--- pkg/cmdline/version.go
+++ pkg/cmdline/version.go
@@ -1,8 +1,30 @@
 package cmdline
 
+import (
+	"runtime/debug"
+)
+
 // set using the '-X github.com/crc-org/vfkit/pkg/cmdline.gitVersion' linker flag
 var gitVersion = "unknown"
 
 func Version() string {
-	return gitVersion
+	switch {
+	// This will be set when building from git using make
+	case gitVersion != "":
+		return gitVersion
+	// moduleVersionFromBuildInfo() will be set when using `go install`
+	default:
+		return moduleVersionFromBuildInfo()
+	}
+}
+
+func moduleVersionFromBuildInfo() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+	if info.Main.Version == "(devel)" {
+		return ""
+	}
+	return info.Main.Version
 }
