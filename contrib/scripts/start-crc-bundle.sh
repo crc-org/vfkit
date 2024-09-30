005ef3e2f7b6ebdafea1efc23a73e2d9b9ac6a56
Christophe Fergeau <cfergeau@redhat.com>
2024-09-30 16:51:00 +0200

    doc: Add documentation for start-crc-bundle.sh
    Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

--- contrib/scripts/start-crc-bundle.sh
+++ contrib/scripts/start-crc-bundle.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# This script can be used to start a
+# [CRC/OpenShift Local bundle](https://crc.dev/blog/)
+# with vfkit.
+# It expects the bundle to be unpacked in ~/.crc/cache.
+# It creates an overlay for the disk image, the files in ~/.crc/cache are not
+# modified.
+#
+# Once the VM is running, you can connect to it using the `id_*_crc` SSH key
+# in the bundle directory. The default user is `core`.
+# The VM IP can be found in `/var/db/dhcpd_leases` by searching for the VM MAC
+# address (72:20:43:d4:38:62)
+#
+# Example:
+# $ sh contrib/scripts/start-crc-bundle.sh ~/.crc/cache/crc_microshift_vfkit_4.16.7/
+# $ ssh -i ~/.crc/cache/crc_microshift_vfkit_4.16.7/id_ecdsa_crc core@192.168.64.2
+
 set -exu
 
 YQ=${YQ:-yq}
