c5fca9bc167aa0e05b3fef83e7176909f562255f
Luca Stocchi <luca@MacBook-Pro-di-Luca.local>
2024-10-02 16:37:37 +0200

    contrib: update start-crc-bundle to use UEFI boot
    it removes all the legacy kernel related options (kernel, initrd, kernel-cmdline) and replace them with the --bootloader efi.

--- contrib/scripts/start-crc-bundle.sh
+++ contrib/scripts/start-crc-bundle.sh
@@ -4,16 +4,11 @@ set -exu
 
 YQ=${YQ:-yq}
 BUNDLE_PATH=$1
-KERNEL=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .nodes[0].kernel)
-INITRD=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .nodes[0].initramfs)
-CMDLINE=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .nodes[0].kernelCmdLine)
 DISKIMG=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .storage.diskImages[0].name)
 cp -c ${BUNDLE_PATH}/${DISKIMG} overlay.img
 
 ./out/vfkit --cpus 2 --memory 2048 \
-    --kernel "${BUNDLE_PATH}/${KERNEL}" \
-    --initrd "${BUNDLE_PATH}/${INITRD}" \
-    --kernel-cmdline "${CMDLINE}" \
+    --bootloader efi,variable-store=efistore.nvram,create \
     --device virtio-blk,path=overlay.img \
     --device virtio-serial,logFilePath=start-bundle.log \
     --device virtio-net,nat,mac=72:20:43:d4:38:62 \
