aeeb618600d65792d7764bee7d5024a6f3986382
Gunjan Vyas <vyasgun20@gmail.com>
2025-04-30 11:11:02 +0530

    Add Validate() to VirtioDevice interface
    This method allows each VirtioDevice implementation to perform its own
    validation logic before being attached to the VM. It helps catch
    misconfigurations early and ensures that invalid devices don't cause
    runtime errors during VM startup.

--- pkg/config/virtio.go
+++ pkg/config/virtio.go
@@ -11,7 +11,10 @@ import (
 )
 
 // The VirtioDevice interface is an interface which is implemented by all virtio devices.
-type VirtioDevice VMComponent
+type VirtioDevice interface {
+	VMComponent
+	Validate() error
+}
 
 const (
 	// Possible values for VirtioInput.InputType
@@ -144,6 +147,10 @@ func (v *VirtioBalloon) ToCmdLine() ([]string, error) {
 	return []string{"--device", "virtio-balloon"}, nil
 }
 
+func (v *VirtioBalloon) Validate() error {
+	return nil
+}
+
 type option struct {
 	key   string
 	value string
@@ -240,7 +247,7 @@ func VirtioSerialNewPty() (VirtioDevice, error) {
 	}, nil
 }
 
-func (dev *VirtioSerial) validate() error {
+func (dev *VirtioSerial) Validate() error {
 	if dev.LogFile != "" && dev.UsesStdio {
 		return fmt.Errorf("'logFilePath' and 'stdio' cannot be set at the same time")
 	}
@@ -258,7 +265,7 @@ func (dev *VirtioSerial) validate() error {
 }
 
 func (dev *VirtioSerial) ToCmdLine() ([]string, error) {
-	if err := dev.validate(); err != nil {
+	if err := dev.Validate(); err != nil {
 		return nil, err
 	}
 	switch {
@@ -290,7 +297,7 @@ func (dev *VirtioSerial) FromOptions(options []option) error {
 		}
 	}
 
-	return dev.validate()
+	return dev.Validate()
 }
 
 // VirtioInputNew creates a new input device for the virtual machine.
@@ -300,14 +307,14 @@ func VirtioInputNew(inputType string) (VirtioDevice, error) {
 	dev := &VirtioInput{
 		InputType: inputType,
 	}
-	if err := dev.validate(); err != nil {
+	if err := dev.Validate(); err != nil {
 		return nil, err
 	}
 
 	return dev, nil
 }
 
-func (dev *VirtioInput) validate() error {
+func (dev *VirtioInput) Validate() error {
 	if dev.InputType != VirtioInputPointingDevice && dev.InputType != VirtioInputKeyboardDevice {
 		return fmt.Errorf("unknown option for virtio-input devices: %s", dev.InputType)
 	}
@@ -316,7 +323,7 @@ func (dev *VirtioInput) validate() error {
 }
 
 func (dev *VirtioInput) ToCmdLine() ([]string, error) {
-	if err := dev.validate(); err != nil {
+	if err := dev.Validate(); err != nil {
 		return nil, err
 	}
 
@@ -335,7 +342,7 @@ func (dev *VirtioInput) FromOptions(options []option) error {
 			return fmt.Errorf("unknown option for virtio-input devices: %s", option.key)
 		}
 	}
-	return dev.validate()
+	return dev.Validate()
 }
 
 // VirtioGPUNew creates a new gpu device for the virtual machine.
@@ -351,7 +358,7 @@ func VirtioGPUNew() (VirtioDevice, error) {
 	}, nil
 }
 
-func (dev *VirtioGPU) validate() error {
+func (dev *VirtioGPU) Validate() error {
 	if dev.Height < 1 || dev.Width < 1 {
 		return fmt.Errorf("invalid dimensions for virtio-gpu device resolution: %dx%d", dev.Width, dev.Height)
 	}
@@ -360,7 +367,7 @@ func (dev *VirtioGPU) validate() error {
 }
 
 func (dev *VirtioGPU) ToCmdLine() ([]string, error) {
-	if err := dev.validate(); err != nil {
+	if err := dev.Validate(); err != nil {
 		return nil, err
 	}
 
@@ -394,7 +401,7 @@ func (dev *VirtioGPU) FromOptions(options []option) error {
 		dev.Height = defaultVirtioGPUResolutionHeight
 	}
 
-	return dev.validate()
+	return dev.Validate()
 }
 
 // VirtioNetNew creates a new network device for the virtual machine. It will
@@ -430,7 +437,7 @@ func (dev *VirtioNet) SetUnixSocketPath(path string) {
 	dev.Nat = false
 }
 
-func (dev *VirtioNet) validate() error {
+func (dev *VirtioNet) Validate() error {
 	if dev.Nat && dev.Socket != nil {
 		return fmt.Errorf("'nat' and 'fd' cannot be set at the same time")
 	}
@@ -448,7 +455,7 @@ func (dev *VirtioNet) validate() error {
 }
 
 func (dev *VirtioNet) ToCmdLine() ([]string, error) {
-	if err := dev.validate(); err != nil {
+	if err := dev.Validate(); err != nil {
 		return nil, err
 	}
 
@@ -497,7 +504,7 @@ func (dev *VirtioNet) FromOptions(options []option) error {
 		}
 	}
 
-	return dev.validate()
+	return dev.Validate()
 }
 
 // VirtioRngNew creates a new random number generator device to feed entropy
@@ -514,6 +521,10 @@ func (dev *VirtioRng) FromOptions(options []option) error {
 	if len(options) != 0 {
 		return fmt.Errorf("unknown options for virtio-rng devices: %s", options)
 	}
+	return nil
+}
+
+func (dev *VirtioRng) Validate() error {
 	return nil
 }
 
@@ -641,6 +652,10 @@ func (dev *VirtioVsock) FromOptions(options []option) error {
 	return nil
 }
 
+func (dev *VirtioVsock) Validate() error {
+	return nil
+}
+
 // VirtioFsNew creates a new virtio-fs device for file sharing. It will share
 // the directory at sharedDir with the virtual machine. This directory can be
 // mounted in the VM using `mount -t virtiofs mountTag /some/dir`
@@ -675,6 +690,10 @@ func (dev *VirtioFs) FromOptions(options []option) error {
 			return fmt.Errorf("unknown option for virtio-fs devices: %s", option.key)
 		}
 	}
+	return nil
+}
+
+func (dev *VirtioFs) Validate() error {
 	return nil
 }
 
@@ -720,6 +739,10 @@ func (dev *RosettaShare) FromOptions(options []option) error {
 			return fmt.Errorf("unknown option for rosetta share: %s", option.key)
 		}
 	}
+	return nil
+}
+
+func (dev *RosettaShare) Validate() error {
 	return nil
 }
 
@@ -801,6 +824,10 @@ func (nbd *NetworkBlockDevice) FromOptions(options []option) error {
 	return nbd.NetworkBlockStorageConfig.FromOptions(unhandledOpts)
 }
 
+func (nbd *NetworkBlockDevice) Validate() error {
+	return nil
+}
+
 type USBMassStorage struct {
 	DiskStorageConfig
 }
@@ -871,6 +898,10 @@ func (config *DiskStorageConfig) FromOptions(options []option) error {
 			return fmt.Errorf("unknown option for %s devices: %s", config.DevName, option.key)
 		}
 	}
+	return nil
+}
+
+func (config *DiskStorageConfig) Validate() error {
 	return nil
 }
 
